37e6025560064418843b6cf7db77270fdf8d9e32
Chun Shen <chunshen@wayne.edu>
2025-03-24 16:35:56 -0400

    update the emulator files

--- downloadEmulators.sh
+++ downloadEmulators.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-curl -L -o emulators.tar.gz "https://www.dropbox.com/scl/fi/pml7zmyrgp2ellbyrm26o/emulators.tar.gz?rlkey=8gg4p20z3h87p2q7kxhk0x91f&st=nqjig9s5&dl=0"
+#curl -L -o emulators.tar.gz "https://www.dropbox.com/scl/fi/pml7zmyrgp2ellbyrm26o/emulators.tar.gz?rlkey=8gg4p20z3h87p2q7kxhk0x91f&st=nqjig9s5&dl=0"
+curl -L -o emulators.tar.gz "https://www.dropbox.com/scl/fi/o9je6zi63amck1kvsodyy/emulators.tar.gz?rlkey=sef2nrs9be2alvu2drcenkmt0&dl=0"
+
 tar -xf emulators.tar.gz
 rm emulators.tar.gz
